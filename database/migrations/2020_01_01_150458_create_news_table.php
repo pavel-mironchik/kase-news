0e72fd9d05e0b0a8b159a9d886b75e289238b49d
Pavel Mironchik <mironchikpavel@gmail.com>
2020-01-01 22:17:09 +0600

    Add News entity.

---
+++ database/migrations/2020_01_01_150458_create_news_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('news', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->dateTime('date_time');
+            $table->string('url');
+            $table->text('title');
+            $table->text('content')
+                ->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('news');
+    }
+}
