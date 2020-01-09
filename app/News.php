0510e0ac1adfd7f9f0ed570c946ba61ac3944558
Pavel Mironchik <mironchikpavel@gmail.com>
2020-01-10 02:21:38 +0600

    Add Message entity.

--- app/News.php
+++ app/News.php
@@ -3,8 +3,17 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class News extends Model
 {
     protected $fillable = ['date_time', 'url', 'title', 'content'];
+
+    /**
+     * @return HasMany
+     */
+    public function messages()
+    {
+        return $this->hasMany(Message::class);
+    }
 }
---
+++ database/migrations/2020_01_09_220423_create_messages_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('news_id');
+            $table->text('content');
+            $table->unsignedBigInteger('message_id')
+                ->nullable();
+            $table->timestamps();
+
+            $table->foreign('news_id')
+                ->references('id')
+                ->on('news')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+}
